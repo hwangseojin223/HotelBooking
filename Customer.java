2ed658a14de69362b7227bbffa13c672fa600a52
hyoyeon <kimhyoyeon75@gmail.com>
2025-03-14 17:55:01 +0900

    add Customer.java

---
+++ Customer.java
@@ -0,0 +1,20 @@
+public class Customer {
+  private String name;
+  private String phoneNumber;
+  private String roomType;
+  private int roomNumber;
+  
+  public Customer() {}
+
+public Customer(String name, String phoneNumber, String roomType) {
+	super();
+	this.name = name;
+	this.phoneNumber = phoneNumber;
+	this.roomType = roomType;
+	this.room
+}
+  
+  
+  
+  
+}
