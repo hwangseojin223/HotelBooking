bd0ace9fb8ecd91cb57dce930dc08f4d7b8e7798
hwangseojin223 <hsj6421@naver.com>
2025-03-15 13:27:15 +0900

    update Customer.java

--- Customer.java
+++ Customer.java
@@ -3,16 +3,35 @@ public class Customer {
 	private String phoneNumber;
 	private String roomType;
 	private int roomNumber;
+	private String day;
 	
 	public Customer() {}
 
-	public Customer(String name, String phoneNumber, String roomType) {
+	public Customer(String name, String phoneNumber, String roomType, int roomNumber, String day) {
 		super();
 		this.name = name;
 		this.phoneNumber = phoneNumber;
 		this.roomType = roomType;
+		this.roomNumber = roomNumber;
+		this.day = day;
 		
 	} 
+
+	public String getName() {
+		return roomType;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
 	public String getRoomType() {
 		return roomType;
 	}
@@ -28,4 +47,12 @@ public class Customer {
 	public void setRoomNumber(int roomNumber) {
 		this.roomNumber = roomNumber;
 	}
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
 }
