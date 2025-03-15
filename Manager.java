fff22165b058c2ba2f079cccc0f4afd53d9905cd
hyj <hyj01117@gmail.com>
2025-03-15 14:17:53 +0900

    update Manager

--- Manager.java
+++ Manager.java
@@ -1,4 +1,20 @@
 public class Manager{
     private Room arr[][];
     private int count;
+
+    public Manager(){
+        arr = new Room[9][31];
+    }
+
+    public void reservation(){
+
+    }
+
+    public void cancel(){
+
+    }
+
+    public void checkout(){
+        
+    }
 }
