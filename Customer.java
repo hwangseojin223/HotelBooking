8a24362b37dab9a226f4f01fd895b795a0190ef3
HYOYEON KIM <kimhyoyeon75@gmail.com>
2025-03-15 13:08:30 +0900

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -12,9 +12,21 @@ public Customer(String name, String phoneNumber, String roomType) {
 	this.phoneNumber = phoneNumber;
 	this.roomType = roomType;
 	
+} 
+public String getRoomType() {
+	return roomType;
 }
-  
-  
-  
-  
+
+public void setRoomType(String roomType) {
+	this.roomType = roomType;
 }
+
+public int getRoomNumber() {
+	return roomNumber;
+}
+
+public void setRoomNumber(int roomNumber) {
+	this.roomNumber = roomNumber;
+}
+}
+
