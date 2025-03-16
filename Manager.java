848252112f543f01fce3ff4e13522fb3dd4342d7
hwangseojin223 <hsj6421@naver.com>
2025-03-16 20:33:43 +0900

    update Manager.java

--- Manager.java
+++ Manager.java
@@ -17,11 +17,11 @@ public class Manager{
         String phoneNumber = sc.nextLine();
 
         String roomType;
-        int roomNumber;
+        int roomNumber = -1;
 
         // 객실 타입 입력
         do{
-            System.out.print("객실 타입을 입력하세요(스탠다드룸, 스위트룸)");
+            System.out.print("객실 타입을 입력하세요(스탠다드룸, 스위트룸): ");
             roomType = sc.nextLine();
             if (roomType.equals("스탠다드룸") || roomType.equals("스위트룸")) break;
             else System.out.print("잘못된 객실 타입입니다."); 
@@ -30,7 +30,7 @@ public class Manager{
         
         if(roomType.equals("스탠다드룸")){
             while(true){
-                System.out.print("객실 번호를 입력하세요(101~105)");
+                System.out.print("객실 번호를 입력하세요(101~105): ");
                 int min = 101;
                 int max = 105;
                 roomNumber = Integer.parseInt(sc.nextLine());
@@ -41,7 +41,7 @@ public class Manager{
         }
         else if(roomType.equals("스위트룸")){
             while(true){
-                System.out.print("객실 번호를 입력하세요(106~109)");
+                System.out.print("객실 번호를 입력하세요(106~109): ");
                 int min = 106;
                 int max = 109;
                 roomNumber = Integer.parseInt(sc.nextLine());
@@ -66,14 +66,16 @@ public class Manager{
             System.out.println("예약이 완료되었습니다");
             //예약번호 출력
             String reservationNum = phoneNumber + day + roomNumber;
-            System.out.println("예약번호:"+reservationNum);
+            
 
             Customer customer = new Customer(name, phoneNumber, roomType, roomNumber, day, personNumber);
             customer.show();
+            System.out.println("예약번호:"+reservationNum);
         } 
         //arr[호수][날짜] 
 
         else if(arr[FixRoomNumber][day-1] != null ){ //비어있지 않으면 빈 방 호수 출력
+            /* 
             System.out.println("예약이 불가능합니다.");
             if (roomType.equals("스탠다드룸")){
                 for(int i=0; i<5; i++){
@@ -85,6 +87,46 @@ public class Manager{
                     if(arr[i][day-1]==null) System.out.println((i+101)+"호 예약 가능");
                 }
             }
+                */
+                System.out.println("예약이 불가능합니다.");
+                boolean roomSelected = false;
+            
+                while (!roomSelected) {
+                    if (roomType.equals("스탠다드룸")){
+                        for(int i=0; i<5; i++){
+                            if(arr[i][day-1] == null) System.out.println((i+101)+"호 예약 가능");
+                        }
+                        System.out.print("다시 객실 번호를 입력하세요(101~105): ");
+                    }
+                    else if (roomType.equals("스위트룸")){
+                        for(int i=5; i<9; i++){
+                            if(arr[i][day-1] == null) System.out.println((i+101)+"호 예약 가능");
+                        }
+                        System.out.print("다시 객실 번호를 입력하세요(106~109): ");
+                    }
+            
+                    roomNumber = Integer.parseInt(sc.nextLine());
+                    FixRoomNumber = roomNumber - 101;
+            
+                    if (arr[FixRoomNumber][day-1] == null) {
+                        roomSelected = true; // 예약 가능한 방이 선택되었을 때 루프 종료
+                    } else {
+                        System.out.println("이미 예약된 방입니다. 다시 선택해주세요.");
+                    }
+                }
+            
+                // 예약 처리
+                if(roomType.equals("스탠다드룸")) arr[FixRoomNumber][day-1] = new StandardRoom();
+                else arr[FixRoomNumber][day-1] = new SuiteRoom();
+            
+                System.out.println("예약이 완료되었습니다");
+                // 예약번호 출력
+                String reservationNum = phoneNumber + day + roomNumber;
+                
+            
+                Customer customer = new Customer(name, phoneNumber, roomType, roomNumber, day, personNumber);
+                customer.show();
+                System.out.println("예약번호:"+reservationNum);
 
         }
     }
