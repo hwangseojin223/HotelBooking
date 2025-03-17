b08b4f599778332817a2209b5ddf1ee1401df162
hyj <hyj01117@gmail.com>
2025-03-17 10:58:23 +0900

    update Manager

--- Manager.java
+++ Manager.java
@@ -41,7 +41,7 @@ public class Manager{
                 roomNumber = Integer.parseInt(sc.nextLine());
 
                 if((roomNumber>=min)&&(roomNumber<=max)) break;
-                else System.out.print("101~105 사이로 입력하세요");
+                else System.out.println("101~105 사이로 입력하세요");
             }   
         }
         else if(roomType.equals("스위트룸")){
